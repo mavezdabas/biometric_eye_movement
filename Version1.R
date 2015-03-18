72e01f628355b37936d9638a117e044fbacf64ed
mavezdabas <dabas.m@husky.neu.edu>
2015-03-18 01:03:21 -0400

    Commit
    Got the One file to the Data Table

--- Version1.R
+++ Version1.R
@@ -9,7 +9,12 @@ eye1 <- eye1[-1,]
 eye2 <- eye1[c(-5,-6,-7,-8,-9,-10)]
 
 
-eye.txt <- read.table(file = "ID_001_1.txt",
-                 # header = FALSE,
+eye.txt <- read.table("ID_001_1.txt",
+                 header = TRUE,
+                 fill = TRUE
                  # sep = ""
 )
+eye.txt.2 <- eye.txt[c(-5,-6,-7,-8,-9,-10)]
+colnames(eye.txt.2) <- c("SAMPLE","X.DEGREE","Y.DEGREE","VALIDITY")
+
+# Omit the Validity avlue that is 1.
